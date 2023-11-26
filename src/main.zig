de6596640d82424e35d42b1d8b101df88654e527
varadal <lalit.varada@gmail.com>
2023-11-25 23:58:36 -0500

    Start of command handling

--- src/main.zig
+++ src/main.zig
@@ -13,14 +13,49 @@ pub fn main() !void {
 
     while (true) {
         const conn = try server.accept();
-        try conn.stream.writer().print("Beans", .{});
+        try conn.stream.writer().print("Beans\n", .{});
+        try handle(conn.stream);
         conn.stream.close();
     }
-
-    // std.debug.print("All your {s} are belong to us.\n", .{"bean"});
-    // const stdout_file = std.io.getStdOut().writer();
-    // var bw = std.io.bufferedWriter(stdout_file);
-    // const stdout = bw.writer();
-    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
-    // try bw.flush(); // don't forget to flush!
 }
+
+fn handle(stream: net.Stream) !void {
+    var count: u8 = 0;
+
+    const writer = stream.writer();
+    const reader = stream.reader();
+
+    while (true) : (count += 1) {
+        try writer.print("({d}) zig-redis> ", .{count});
+        var buf: [256]u8 = undefined;
+        const hold = try reader.readUntilDelimiter(&buf, '\n');
+
+        if (hold.len > 0) {
+            if ((hold.len == 1) and (hold[0] == 'q')) return;
+            try redis_handle(hold, writer);
+        }
+        if (count > 10) return;
+    }
+}
+
+fn redis_handle(command: []u8, writer: net.Stream.Writer) !void {
+    const idx = std.mem.indexOf(u8, command, " ");
+    if (idx == null) return; // TODO: throw an error
+
+    const cmd = command[0..idx.?];
+    _ = cmd;
+    // std.debug.print("PING {s}", .{cmd});
+    try writer.print("PING {s}\n", .{command[idx.? + 1 ..]});
+}
+
+// TODO:
+// 1. be able to consume values being sent to the server
+//  a. connections should be stored by the server obj
+//  b. how to tell when connections die?
+//  c. how to do async?
+// 2. move the server to its own file
+// 3. get started on the redis part
+
+// NOTE: tcp-server: https://beej.us/guide/bgnet/html/#a-simple-stream-server
+// NOTE: youtube video about creating a server in zig: https://www.youtube.com/watch?v=olOJbYP0ORE&list=PLS87DlLl8etzu2yg5c6a8dDB3wntFsRcj
+// NOTE: redis plan: https://app.codecrafters.io/courses/redis/overview
---
+++ src/redis.zig
@@ -0,0 +1,5 @@
+// const std = @import("std");
+//
+// pub const redis = struct {
+//     // add a concurrent hashmap
+// };
